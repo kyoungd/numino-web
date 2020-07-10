d8c57cb9ed7f1399b67dceb92059e991a35814ae
young <kyoungd@hotmail.com>
2020-07-10 11:25:34 -0700

    added test data support

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,6 +1,6 @@
 export { default as chartjs } from './chartjs';
 export { default as getInitials } from './getInitials';
-export const serverUrl = 'https://strapi-first-app-1.herokuapp.com/';
+export const serverUrl = 'http://localhost:1337/';
 export const numberWithCommas = x => {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
